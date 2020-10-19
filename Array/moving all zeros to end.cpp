c58890b4c5a8b545b9d38fea9c04b5d05a07d9ed
tushar khandelwal <tusharkhandelwal315@gmail.com>
2020-10-19 12:54:07 +0530

    moving all zeros to end

---
+++ Array/moving all zeros to end.cpp
@@ -0,0 +1,16 @@
+//move all zeros to end without changing the order of non zero elements
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	vector <int> arr={1,4,0,2,3,0,7,0,0,0,4};
+	int nzidz=-1;
+	for(int i=0;i<arr.size();i++)
+	{
+		if(arr[i]!=0)swap(arr[++nzidz],arr[i]);
+	}
+	for(auto e:arr)cout<<e<<" ";
+
+	return 0;
+}
